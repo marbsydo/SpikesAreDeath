0e001853d4f1492ee8364554d08ba770cc3a53ec
Sam Hooke <marbsam@yahoo.co.uk>
2013-10-31 22:04:50 +0000

    Rewritten spike section generation
    Spikes are generated from a string. Each section has a difficulty
    variable.

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public enum Side{E, L, R}
+public enum SectionDifficulty{SuperEasy, Easy, Medium, Hard, SuperHard}
 
 public class Section {
 	public bool spikeLeft;
@@ -11,9 +12,8 @@ public class Section {
 
 public class LevelGenerator : MonoBehaviour {
 
-	
-
 	Queue<Section> gen = new Queue<Section>();
+	List<SectionBlueprint> blueprints = new List<SectionBlueprint>();
 
 	float speed = 4.0f;
 	float grav = 4.0f;
@@ -25,15 +25,16 @@ public class LevelGenerator : MonoBehaviour {
 	GameObject lastObject;
 
 	float timeLast;
-	float timeGap = 0.35f;
 	
 	Vector3 posLeft = new Vector3(-2f, -10, 0);
 	Vector3 posRight = new Vector3(2f, -10, 0);
 
-	Dictionary<Side, float> sideWeights = new Dictionary<Side, float>();
 	public Side currentSide = Side.E;
+	Dictionary<Side, float> sideWeights = new Dictionary<Side, float>();
+	Dictionary<SectionDifficulty, float> difficultyWeights = new Dictionary<SectionDifficulty, float>();
 
 	// Level generation modifiers
+	float timeGap = 0.35f;                                  // The time in seconds between generating spikes. Minimum = 0.35f
 	float globalSpeedModifier = 1.0f;                       // Affects speed of all game related objects including spikes and player
 	float spikeDensity = 1.0f;                              // 1.0f = maximum density spikes, 0.0f = no spikes
 	bool changeWeightsToMakeSideSwappingMoreLikely = false; // If true, sequences of multiple identical spikes are made less unlikely
@@ -42,9 +43,24 @@ public class LevelGenerator : MonoBehaviour {
 		spikeLeft = Resources.Load("SpikeLeft");
 		spikeRight = Resources.Load("SpikeRight");
 
+		// Weights for which side the player should be on
 		sideWeights.Add(Side.E, 1.0f);
 		sideWeights.Add(Side.L, 1.0f);
 		sideWeights.Add(Side.R, 1.0f);
+
+		// Weights for various difficults of spike generation
+		difficultyWeights.Add(SectionDifficulty.SuperEasy, 1.0f);
+		difficultyWeights.Add(SectionDifficulty.Easy,      0.0f);
+		difficultyWeights.Add(SectionDifficulty.Medium,    0.0f);
+		difficultyWeights.Add(SectionDifficulty.Hard,      0.0f);
+		difficultyWeights.Add(SectionDifficulty.SuperHard, 0.0f);
+
+		// Create blueprints
+		blueprints.Add(new SectionBlueprint(this, Side.E, Side.E, SectionDifficulty.SuperEasy, "n"));
+		blueprints.Add(new SectionBlueprint(this, Side.L, Side.L, SectionDifficulty.SuperEasy, "nrn"));
+		blueprints.Add(new SectionBlueprint(this, Side.R, Side.R, SectionDifficulty.SuperEasy, "nln"));
+		blueprints.Add(new SectionBlueprint(this, Side.L, Side.L, SectionDifficulty.SuperEasy, "nrrrn"));
+		blueprints.Add(new SectionBlueprint(this, Side.R, Side.R, SectionDifficulty.SuperEasy, "nllln"));
 	}
 
 	void OnLevelWasLoaded() {
@@ -100,27 +116,10 @@ public class LevelGenerator : MonoBehaviour {
 				currentSide = nextSide;
 				v = Random.value;
 				if (v <= spikeDensity) {
-					v = Random.value;
-					switch (currentSide) {
-					case Side.E:
-						GenSpikes(true, true);
-						GenSpikes(false, false);
-						GenSpikes(false, false);
-						break;
-					case Side.L:
-						GenSpikes(false, true);
-						/*
-						GenSpikes(false, true);
-						GenSpikes(true, false);
-						GenSpikes(true, false);
-						GenSpikes(false, true);
-						GenSpikes(false, true);
-						*/
-						break;
-					case Side.R:
-						GenSpikes(true, false);
-						break;
-					}
+					// Call AddSomeSpikes to add some spikes
+					// Tell it what the current side is
+					// It will then return the new side we are at
+					currentSide = AddSomeSpikes(currentSide);
 				}
 
 				// Change weights to make side swapping more likely
@@ -138,11 +137,61 @@ public class LevelGenerator : MonoBehaviour {
 			// Generate the thing at the front of the queue and remove it
 			GenGenerate();
 
-			Debug.Log("E: " + sideWeights[Side.E] + " L: " + sideWeights[Side.L] + " R: " + sideWeights[Side.R]);
+			//Debug.Log("E: " + sideWeights[Side.E] + " L: " + sideWeights[Side.L] + " R: " + sideWeights[Side.R]);
 		}
 	}
 
-	void GenSpikes(bool spikeLeft, bool spikeRight) {
+	Side AddSomeSpikes(Side thisSide) {
+		Side nextSide = Side.E;
+		SectionDifficulty chosenDifficulty = SectionDifficulty.Medium;
+
+		// Choose what difficulty this section will be based upon the weights
+		float w_se = difficultyWeights[SectionDifficulty.SuperEasy];
+		float w_e = difficultyWeights[SectionDifficulty.Easy];
+		float w_m = difficultyWeights[SectionDifficulty.Medium];
+		float w_h = difficultyWeights[SectionDifficulty.Hard];
+		float w_sh = difficultyWeights[SectionDifficulty.SuperHard];
+		float v = Random.value * (w_se + w_e + w_m + w_h + w_sh);
+
+		if (v < w_se) {
+			chosenDifficulty = SectionDifficulty.SuperEasy;
+		} else if (v < (w_se + w_e)) {
+			chosenDifficulty = SectionDifficulty.Easy;
+		} else if (v < (w_se + w_e + w_m)) {
+			chosenDifficulty = SectionDifficulty.Medium;
+		} else if (v < (w_se + w_e + w_m + w_h)) {
+			chosenDifficulty = SectionDifficulty.Hard;
+		} else {
+			chosenDifficulty = SectionDifficulty.SuperHard;
+		}
+
+		// We have chosen a difficulty
+		// Now find all blueprints that match this difficulty and side
+		List<int> goodBlueprints = new List<int>();
+		for (int i = 0; i < blueprints.Count; i++) {
+			if (blueprints[i].difficulty == chosenDifficulty && blueprints[i].sideIn == thisSide)
+				goodBlueprints.Add(i);
+		}
+
+		// Choose a random blueprint from the list of good blueprints
+		if (goodBlueprints.Count == 0) {
+			// Found no blueprints; show warning and generate no spikes
+			Debug.LogWarning("Could not find any appropriate blueprints!");
+			
+			GenSpikes(false, false);
+			nextSide = Side.E;
+		} else {
+			// Choose a random blueprint from the list of IDs in goodBlueprints
+			SectionBlueprint chosenBlueprint = blueprints[goodBlueprints[Random.Range(0, goodBlueprints.Count)]];
+
+			chosenBlueprint.Generate();
+			nextSide = chosenBlueprint.sideOut;
+		}
+
+		return nextSide;
+	}
+
+	public void GenSpikes(bool spikeLeft, bool spikeRight) {
 		Section s = new Section();
 		s.spikeLeft = spikeLeft;
 		s.spikeRight = spikeRight;
@@ -174,3 +223,55 @@ public class LevelGenerator : MonoBehaviour {
 		obj.GetComponent<Spike>().lg = this;
 	}
 }
+
+class SectionBlueprint {
+	private LevelGenerator lg;
+	public Side sideIn;
+	public Side sideOut;
+	public SectionDifficulty difficulty;
+	private string spikes;
+
+	// e.g. new SectionBlueprint(Side.L, Side.R, SectionDifficulty.Medium, "rrnbnnbnll")
+	public SectionBlueprint(LevelGenerator lg, Side sideIn, Side sideOut, SectionDifficulty difficulty, string spikes) {
+		this.lg = lg;
+		this.sideIn = sideIn;
+		this.sideOut = sideOut;
+		this.difficulty = difficulty;
+		this.spikes = spikes;
+	}
+
+	public void Generate() {
+		// Use spikes to generate spikes
+		// Spikes is e.h. "lnrnnbnn"
+
+		for (int i = 0; i < spikes.Length; i++) {
+			switch (spikes[i]) {
+			case 'l':	lg.GenSpikes(true, false);		break;
+			case 'n':	lg.GenSpikes(false, false);	break;
+			case 'r':	lg.GenSpikes(false, true);		break;
+			case 'b':	lg.GenSpikes(true, true);		break;
+			}
+		}
+	}
+}
+
+/*
+abstract class SpikeSectionBase {
+	public Side sideIn;
+	public Side sideOut;
+	public SectionDifficulty difficulty;
+	public abstract void Generate();
+}
+
+class SS_EtoE_SuperEasy_1 {
+	SS_EtoE_SuperEasy_1 {
+		sideIn = Side.E;
+		sideOut = Side.E;
+		difficulty = SectionDifficulty.SuperEasy;
+	}
+
+	public override void Generate() {
+		GenSpikes(false, false);
+	}
+}
+*/
